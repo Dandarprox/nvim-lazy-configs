c9b8a7174622402e2c3da36cce493287f62d13dd
Daniel Rodríguez Meza <dlsusp@gmail.com>
2024-01-16 11:00:55 -0500

    feat: add basic nvim configs with lazy

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,26 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
+
+local function add_undo_point()
+  vim.cmd("silent! undojoin | normal! a")
+end
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = "*",
+  callback = function()
+    add_undo_point()
+  end,
+})
+
+local brackets = { ["["] = "]", ["{"] = "}", ["("] = ")" }
+for open, close in pairs(brackets) do
+  vim.api.nvim_set_keymap("i", open, open .. close .. "<Left><C-g>u", { noremap = true, silent = true })
+end
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "vue" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    enabled = false,
+    opts = {
+      indent = { char = "" },
+    },
+    scope = { enabled = false },
+  },
+  {
+    "echasnovski/mini.indentscope",
+    enabled = false,
+  },
+}

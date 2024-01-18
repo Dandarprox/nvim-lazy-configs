001bfcddfeda6ed9a6dc314d91d35d8709ab844d
Daniel Rodríguez Meza <dlsusp@gmail.com>
2024-01-18 11:07:43 -0500

    feat: configure toggleterm

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local Util = require("lazyvim.util")
 
 vim.opt.guicursor = "i:block"
 
@@ -9,3 +10,9 @@ vim.opt.termguicolors = true
 vim.opt.background = "dark"
 vim.g.gruvbox_material_background = "hard"
 vim.g.gruvbox_material_better_performance = 1
+
+vim.opt.list = false
+vim.opt.shell = "pwsh.exe"
+
+-- Disable conceal
+Util.toggle("conceallevel", false, { 0, 0 })
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,5 +3,9 @@ return {
   version = "*",
   opts = {--[[ things you want to change go here]]
     direction = "float",
+    shell = vim.o.shell,
+  },
+  float_opts = {
+    border = "double",
   },
 }

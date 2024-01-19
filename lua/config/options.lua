51bd76737a955757bd29c7e4b87e3445a91cc918
Daniel Rodríguez Meza <dlsusp@gmail.com>
2024-01-18 20:52:32 -0500

    feat: disable conceal

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,8 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-local Util = require("lazyvim.util")
-
 vim.opt.guicursor = "i:block"
 
 vim.opt.termguicolors = true
@@ -15,4 +13,4 @@ vim.opt.list = false
 vim.opt.shell = "pwsh.exe"
 
 -- Disable conceal
-Util.toggle("conceallevel", false, { 0, 0 })
+vim.opt.conceallevel = 0
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,4 +2,8 @@ return {
   "LunarVim/darkplus.nvim",
   lazy = true,
   priority = 1000,
+  opts = {
+    italic = false,
+  },
+  italic = false,
 }
